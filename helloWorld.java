8f866e693d81fc9111e5f20062336363709b2019
isaacmodeyin <modeyinisaac6@gmail.com>
2023-10-06 21:40:09 +0100

    coding in java

--- helloWorld.java
+++
@@ -1,8 +0,0 @@
-
-// A program that prints "Hello, World"
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ javaproject/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World Isaac JAVA!");
+    }
+}
+// ctrl + shift + p  to create java project
