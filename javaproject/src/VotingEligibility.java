85c212fcfb8bef9fd6acc0178e6a0c957f96a954
isaacmodeyin <modeyinisaac6@gmail.com>
2023-11-01 14:21:14 +0100

    Voting Eligibility Program

---
+++ javaproject/src/VotingEligibility.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        int age;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote in Nigeria.");
+        } else {
+            System.out.println("You are not eligible to vote in Nigeria.");
+        }
+    }
+}
